5a67e6ae8639243d06a29654e6cb74cacec91022
Saturn Computing Topic <hatsanmobiletopic@gmail.com>
2025-03-07 17:45:03 -0800

    Add files via upload

---
+++ ListData.py
@@ -0,0 +1,23 @@
+import sys
+import time
+import os
+
+Dist = ["YanderePy.exe.py","YanderePy.exe.exe",]
+print(Dist[0])
+print(Dist[1])
+time.sleep(4)
+os.system("Ping youtube.com")
+time.sleep(5)
+List = ["RUN","PRINT"]
+print(List[0])
+print(List[1])
+RELIGON = "catholic"
+time.sleep(5)
+print(f"Total data is {Dist} {List} {RELIGON}")
+DIM = 10
+VAR = 6
+x = DIM + VAR
+y = x / 4
+print(f"Calculated {x} when DIM is {DIM} and VAR{VAR} and when divided by 4 result:{y}")
+time.sleep(4)
+sys.exit()
